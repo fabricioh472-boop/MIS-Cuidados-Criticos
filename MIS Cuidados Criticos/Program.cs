70c34b52b92348ddc47481ca169be02c19a3d9de
mflehaeior243 <hriojaafabrici@gmail.com>
2026-05-05 17:37:10 -0400

    bug de conexion arreglado

--- MIS Cuidados Criticos/Program.cs
+++ MIS Cuidados Criticos/Program.cs
@@ -3,30 +3,11 @@ using MIS_Cuidados_Criticos.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 🔥 Obtener conexión correctamente
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
-
 // DB
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 🔥 Puerto correcto para Railway
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CuidadosCriticos",
-        builder =>
-        {
-            builder.AllowAnyOrigin();
-            builder.AllowAnyMethod();
-            builder.AllowAnyHeader();
-        });
-});
-
-// Servicios
+//Constructores de servicios
 builder.Services.AddHttpClient<EnfermeriaService>();
 builder.Services.AddHttpClient<LogisticaService>();
 builder.Services.AddControllers();
@@ -40,10 +21,5 @@ app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
-}
 
 app.Run();
