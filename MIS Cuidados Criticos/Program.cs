be05abe2aca4d5301be8eac2d656ad995a60347f
mflehaeior243 <hriojaafabrici@gmail.com>
2026-05-05 18:58:47 -0400

    Mejora en program

--- MIS Cuidados Criticos/Program.cs
+++ MIS Cuidados Criticos/Program.cs
@@ -3,21 +3,46 @@ using MIS_Cuidados_Criticos.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Obtener connection string local primero
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2. Si existe Railway, lo sobreescribe
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+
+    connectionString =
+        $"Host={uri.Host};" +
+        $"Port={uri.Port};" +
+        $"Database={uri.AbsolutePath.Trim('/')};" +
+        $"Username={userInfo[0]};" +
+        $"Password={userInfo[1]};" +
+        $"SSL Mode=Require;Trust Server Certificate=true";
+}
+
 // DB
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
-//Constructores de servicios
+// Services
 builder.Services.AddHttpClient<EnfermeriaService>();
 builder.Services.AddHttpClient<LogisticaService>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Swagger SOLO en desarrollo (recomendado)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 app.MapControllers();
